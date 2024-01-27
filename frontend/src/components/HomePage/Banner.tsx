ec7ae2ee461a0431b6c22dcf5ca98fb1152b0df0
Sk Shariful Islam <freelancershariful10@gmail.com>
2024-01-28 01:02:39 +0600

    Banner Section Right Side Implemented

--- frontend/src/components/HomePage/Banner.tsx
+++ frontend/src/components/HomePage/Banner.tsx
@@ -2,9 +2,32 @@ import React from "react";
 
 const Banner = () => {
 	return (
-		<div className="mt-10 mb-[60px] bg-gradient-to-b from-primary to-[#002562] px-5 py-14 md:px-20 md:py-28 rounded-2xl text-white">
-			<h1 className="text-[40px] leading-[55px] md:text-[72px] font-bold md:w-[60%] md:leading-[82px]">
-				Rejuvenate your innovative thoughts
+		<div className="mt-10 mb-[60px] bg-gradient-to-b from-primary to-[#002562] px-5 py-14 md:px-20 md:py-[120px] rounded-2xl text-white relative overflow-hidden">
+         <div className="absolute -top-20 -right-20 hidden md:grid grid-cols-3 gap-5 rotate-45">
+            <div className="w-[180px] h-[135px] bg-gray-200 rounded-2xl"></div>
+            <div className="w-[180px] h-[135px] bg-gray-200 rounded-2xl"></div>
+            <div className="w-[180px] h-[135px] bg-gray-200 rounded-2xl"></div>
+            <div className="w-[180px] h-[135px] bg-gray-200 rounded-2xl"></div>
+            <div className="w-[180px] h-[135px] bg-gray-200 rounded-2xl"></div>
+            <div className="w-[180px] h-[135px] bg-gray-200 rounded-2xl"></div>
+            <div className="w-[180px] h-[135px] bg-gray-200 rounded-2xl"></div>
+            <div className="w-[180px] h-[135px] bg-gray-200 rounded-2xl"></div>
+            <div className="w-[180px] h-[135px] bg-gray-200 rounded-2xl"></div>
+            <div className="w-[180px] h-[135px] bg-gray-200 rounded-2xl"></div>
+            <div className="w-[180px] h-[135px] bg-gray-200 rounded-2xl"></div>
+            <div className="w-[180px] h-[135px] bg-gray-200 rounded-2xl"></div>
+            <div className="w-[180px] h-[135px] bg-gray-200 rounded-2xl"></div>
+            <div className="w-[180px] h-[135px] bg-gray-200 rounded-2xl"></div>
+            <div className="w-[180px] h-[135px] bg-gray-200 rounded-2xl"></div>
+            <div className="w-[180px] h-[135px] bg-gray-200 rounded-2xl"></div>
+            <div className="w-[180px] h-[135px] bg-gray-200 rounded-2xl"></div>
+            <div className="w-[180px] h-[135px] bg-gray-200 rounded-2xl"></div>
+            <div className="w-[180px] h-[135px] bg-gray-200 rounded-2xl"></div>
+            <div className="w-[180px] h-[135px] bg-gray-200 rounded-2xl"></div>
+         </div>
+			<div className="">
+         <h1 className="text-[40px] leading-[55px] md:text-[72px] font-bold md:w-[60%] md:leading-[82px] uppercase">
+				Lorem ipsum dolor sit amet.
 			</h1>
 			<p className="text-base md:text-lg my-5 md:w-1/2">
 				Have the to of lose he least unmolested receive fixed is the of
@@ -13,6 +36,7 @@ const Banner = () => {
 			<button className="bg-primary hover:bg-[#316dce] transition-all ease-in-out duration-500 font-medium text-white py-3 px-8 rounded-lg mt-5">
 				Explore Now
 			</button>
+         </div>
 		</div>
 	);
 };

b1d1e0937c9204efd698f8cb0b11fbac1eb534d9
Sk Shariful Islam <freelancershariful10@gmail.com>
2024-01-28 23:51:50 +0600

    Latest Items Section Implemented

--- frontend/src/components/HomePage/LatestItems.tsx
+++ frontend/src/components/HomePage/LatestItems.tsx
@@ -1,14 +1,49 @@
-import React from 'react'
-import SectionTitle from '../SectionTitle'
-import SectionDesc from '../SectionDesc'
-import { items } from '../Sidebar'
+import React from "react";
+import SectionTitle from "../SectionTitle";
+import SectionDesc from "../SectionDesc";
+import ImageCard from "../ImageCard";
+
+const items = [
+	{
+		id: "1",
+		title: "all",
+	},
+	{
+		id: "2",
+		title: "graphics",
+	},
+	{
+		id: "3",
+		title: "photography",
+	},
+	{
+		id: "4",
+		title: "video",
+	},
+	{
+		id: "5",
+		title: "audio",
+	},
+];
 
 const LatestItems = () => {
-  return (
-    <div className='mt-10 mb-12'>
-      
-    </div>
-  )
-}
+	return (
+		<div className="mt-10 mb-12">
+			<SectionTitle title="Latest" subtitle="Items" />
+			<SectionDesc description="Browse All Latest Charms" />
+			<div className="mt-5 flex justify-center gap-6">
+				{items.map(({ id, title }) => (
+					<button
+						key={id}
+						className="hover:text-white capitalize bg-gray-100 hover:bg-primary duration-300 transition-all ease-in-out py-3 px-4 rounded-lg"
+					>
+						{title}
+					</button>
+				))}
+			</div>
+         <ImageCard />
+		</div>
+	);
+};
 
-export default LatestItems
+export default LatestItems;
---
+++ frontend/src/components/ImageCard.tsx
@@ -0,0 +1,102 @@
+import React from "react";
+import Image from "next/image";
+import FormattedPrice from "./FormattedPrice";
+
+const images = [
+	{
+		id: 1,
+		image: "https://dummyimage.com/640x420/a8a6a6/fff",
+		title: "Cherry Blossom Nature Photo",
+		price: 12,
+		vendor_name: "Alivia Adkins",
+		category: "Graphics",
+	},
+	{
+		id: 2,
+		image: "https://dummyimage.com/640x420/a8a6a6/fff",
+		title: "Business Brochure Template",
+		price: 14,
+		vendor_name: "Alivia Adkins",
+		category: "Photo",
+	},
+	{
+		id: 3,
+		image: "https://dummyimage.com/640x420/a8a6a6/fff",
+		title: "Business Brochure Template",
+		price: 11,
+		vendor_name: "Alivia Adkins",
+		category: "video",
+	},
+	{
+		id: 4,
+		image: "https://dummyimage.com/640x420/a8a6a6/fff",
+		title: "Business Brochure Template",
+		price: 22,
+		vendor_name: "audio",
+		category: "",
+	},
+	{
+		id: 5,
+		image: "https://dummyimage.com/640x420/a8a6a6/fff",
+		title: "Business Brochure Template",
+		price: 15,
+		vendor_name: "photo",
+		category: "Alivia Adkins",
+	},
+	{
+		id: 6,
+		image: "https://dummyimage.com/640x420/a8a6a6/fff",
+		title: "Business Brochure Template",
+		price: 14,
+		vendor_name: "audio",
+		category: "Alivia Adkins",
+	},
+	{
+		id: 7,
+		image: "https://dummyimage.com/640x420/a8a6a6/fff",
+		title: "Business Brochure Template",
+		price: 12,
+		vendor_name: "Alivia Adkins",
+		category: "video",
+	},
+	{
+		id: 8,
+		image: "https://dummyimage.com/640x420/a8a6a6/fff",
+		title: "Business Brochure Template",
+		price: 16,
+		vendor_name: "Alivia Adkins",
+		category: "photo",
+	},
+];
+
+const ImageCard = () => {
+	return (
+		<div className="mt-10 grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+			{images.map(({ id, image, title, price, vendor_name, category }) => (
+				<div key={id} className="cursor-pointer">
+					<Image
+						src={image}
+						width={500}
+						height={500}
+						className="rounded-xl"
+						alt="image"
+					/>
+					<div className="mt-5 text-textColor">
+						<div className="flex justify-between font-bold">
+							<p className="">{title}</p>
+							<span className="text-primary">
+								<FormattedPrice amount={price} />
+							</span>
+						</div>
+						<p className="">
+							by <span className="font-medium">{vendor_name}</span> in{" "}
+							<span className="font-medium">{category}</span>
+						</p>
+					</div>
+				</div>
+			))}
+		</div>
+	);
+};
+
+export default ImageCard;

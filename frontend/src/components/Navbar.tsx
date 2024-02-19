223956efb38fe7820515d1bc2845cc14d7e70a81
Sk Shariful Islam <freelancershariful10@gmail.com>
2024-02-19 22:42:03 +0600

    Refector: Navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -16,94 +16,92 @@ import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
 interface Props {
-  show: boolean;
-  showCart: boolean;
-  setShow: React.Dispatch<React.SetStateAction<boolean>>;
-  setShowCart: React.Dispatch<React.SetStateAction<boolean>>;
+	show: boolean;
+	showCart: boolean;
+	setShow: React.Dispatch<React.SetStateAction<boolean>>;
+	setShowCart: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const Navbar = ({ show, setShow, showCart, setShowCart }: Props) => {
-  const { data: user, isLoading } = useContext(UserContext);
-  const { logoutUser } = auth;
-  const [open, setOpen] = useState(false);
+	const { data: user, isLoading } = useContext(UserContext);
+	const { logoutUser } = auth;
+	const [open, setOpen] = useState(false);
 
-  return (
-    <div className="md:px-10 py-5 bg-white">
-      <Container>
-        <div className="flex justify-between items-center">
-          <div className="hidden md:flex items-center gap-1">
-            <input
-              type="search"
-              className="border border-textColor outline-none py-2 px-4 rounded-lg"
-              placeholder="e.g popular product"
-              name=""
-              id=""
-            />
-            <button className="bg-primary text-white py-[10px] px-4 rounded-lg">
-              <RiSearchLine size={20} />
-            </button>
-          </div>
-          <Link href="/" className="md:hidden">
-            <Image
-              src={logo}
-              className="w-[70px] rounded-md"
-              alt="logo"
-              width={70}
-              height={70}
-            />
-          </Link>
-          <div className="flex items-center gap-4">
-            <div className="flex items-center gap-4">
-              <div
-                onClick={() => setShowCart(true)}
-                className="relative cursor-pointer"
-              >
-                <IoMdCart className="text-2xl" />
-                <div className="bg-primary rounded-full p-0.5 h-4 w-4 flex justify-center items-center text-white text-[10px] absolute -top-1 -right-1">
-                  0
-                </div>
-              </div>
-              {isLoading ? null : (
-                <div>
-                  {user.userData ? (
-                    <>
-                      <Image
-                        src={user.userData.profileImg}
-                        width={40}
-                        height={40}
-                        onClick={() => setOpen(!open)}
-                        className="rounded-full object-cover h-10 w-10 cursor-pointer"
-                        alt="ProfileImage"
-                      />
-                      <ProfileMenu
-                        open={open}
-                        user={user.userData!}
-                        setOpen={setOpen}
-                        logoutUser={logoutUser}
-                      />
-                    </>
-                  ) : (
-                    <Link href="/login">
-                      <button className="hidden hover:bg-primary transition-all ease-in-out duration-700 py-2 px-5 rounded-lg font-semibold text-black hover:text-white lg:flex items-center gap-2">
-                        <LuUser2 />
-                        Login
-                      </button>
-                    </Link>
-                  )}
-                </div>
-              )}
-            </div>
-            <div className="md:hidden">
-              {!show && (
-                <RxHamburgerMenu onClick={() => setShow(true)} size={24} />
-              )}
-            </div>
-          </div>
-        </div>
-      </Container>
-      <CartPage showCart={showCart} setShowCart={setShowCart} />
-    </div>
-  );
+	return (
+		<div className="md:px-10 py-5 bg-white">
+			<Container>
+				<div className="flex justify-between items-center">
+					<div className="hidden md:flex items-center gap-1">
+						<input
+							type="search"
+							className="border border-textColor outline-none py-2 px-4 rounded-lg"
+							placeholder="e.g popular product"
+							name=""
+							id=""
+						/>
+						<button className="bg-primary text-white py-[10px] px-4 rounded-lg">
+							<RiSearchLine size={20} />
+						</button>
+					</div>
+					<Link href="/" className="md:hidden">
+						<Image
+							src={logo}
+							className="w-[70px] rounded-md"
+							alt="logo"
+							width={70}
+							height={70}
+						/>
+					</Link>
+					<div className="flex items-center gap-4">
+						<div className="flex items-center gap-4">
+							<div
+								onClick={() => setShowCart(true)}
+								className="relative cursor-pointer"
+							>
+								<IoMdCart className="text-2xl" />
+								<div className="bg-primary rounded-full p-0.5 h-4 w-4 flex justify-center items-center text-white text-[10px] absolute -top-1 -right-1">
+									0
+								</div>
+							</div>
+							<div>
+								{user.userData ? (
+									<>
+										<Image
+											src={user.userData.profileImg}
+											width={40}
+											height={40}
+											onClick={() => setOpen(!open)}
+											className="rounded-full object-cover h-10 w-10 cursor-pointer"
+											alt="ProfileImage"
+										/>
+										<ProfileMenu
+											open={open}
+											user={user.userData!}
+											setOpen={setOpen}
+											logoutUser={logoutUser}
+										/>
+									</>
+								) : (
+									<Link href="/login">
+										<button className="hidden hover:bg-primary transition-all ease-in-out duration-700 py-2 px-5 rounded-lg font-semibold text-black hover:text-white lg:flex items-center gap-2">
+											<LuUser2 />
+											Login
+										</button>
+									</Link>
+								)}
+							</div>
+						</div>
+						<div className="md:hidden">
+							{!show && (
+								<RxHamburgerMenu onClick={() => setShow(true)} size={24} />
+							)}
+						</div>
+					</div>
+				</div>
+			</Container>
+			<CartPage showCart={showCart} setShowCart={setShowCart} />
+		</div>
+	);
 };
 
 export default Navbar;

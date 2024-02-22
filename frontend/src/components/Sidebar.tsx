e60c1be82650018593bd153b27255994d903999b
Sk Shariful Islam <freelancershariful10@gmail.com>
2024-02-23 02:18:09 +0600

    Refector: Categories Icon Remove

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -4,48 +4,39 @@ import Link from "next/link";
 import Image from "next/image";
 import logo from "@/images/logo.png";
 import { CgClose } from "react-icons/cg";
-import { TbVideo } from "react-icons/tb";
-import { VscHome } from "react-icons/vsc";
 import { usePathname } from "next/navigation";
-import { HiOutlineColorSwatch } from "react-icons/hi";
-import { LuImagePlus, LuUser2 } from "react-icons/lu";
 import { MdOutlineQueueMusic } from "react-icons/md";
+import { LuUser2 } from "react-icons/lu";
 
 export const items = [
 	{
 		id: "1",
 		title: "home",
-		Icon: VscHome,
 		url: "/",
 	},
 	{
 		id: "2",
 		title: "graphics",
-		Icon: HiOutlineColorSwatch,
 		url: "/graphics",
 	},
 	{
 		id: "3",
 		title: "photos",
-		Icon: LuImagePlus,
 		url: "/photos",
 	},
 	{
 		id: "4",
 		title: "video",
-		Icon: TbVideo,
 		url: "/video",
 	},
 	{
 		id: "5",
 		title: "audio",
-		Icon: MdOutlineQueueMusic,
 		url: "/audio",
 	},
 	{
 		id: "6",
 		title: "blogs",
-		Icon: MdOutlineQueueMusic,
 		url: "/blogs",
 	},
 ];
@@ -83,15 +74,14 @@ const Sidebar = ({ show, setShow }: Props) => {
         </div>
         <div className="my-[30px]">
           <div className="">
-            {items.map(({ id, title, Icon, url }) => (
+            {items.map(({ id, title, url }) => (
               <ul key={id}>
                 <li
                   onClick={() => setShow(false)}
-                  className={`flex items-center gap-4 px-5 py-[13px] cursor-pointer ${
+                  className={`px-5 py-[13px] cursor-pointer ${
                     pathName === url && "text-primary"
                   } hover:text-primary capitalize text-lg font-medium`}
                 >
-                  <Icon className="text-2xl" />
                   <Link href={`${url ? url : ""}`}>{title}</Link>
                 </li>
               </ul>
